acbc4feff710a6cd0a722bde982663f89cf4a606
N ANANDA BABU <babu.nalluri7@gmail.com>
2019-06-26 07:14:26 +0530

    First time commit to my selenium code

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class LoginPage {
+	
+	public void LoginPagee() {
+		
+		System.out.println("login page");
+	}
+
+}
---
+++ src/main/java/com/qa/uitlits/Util.java
@@ -0,0 +1,5 @@
+package com.qa.uitlits;
+
+public class Util {
+
+}
